85e345a3b64903bf5119b47b0c842075a23d145d
lemonia28 <110980454+lemonia28@users.noreply.github.com>
2026-05-17 21:18:16 +0100

    Add files via upload

--- minesweeper.c
+++ minesweeper.c
@@ -4,139 +4,121 @@
 
 int main(int argc, char *argv[])
 {
-  printf("saper\n");
-  printf("\n");
-  printf("\n");
- 
- 
+    printf("Minesweeper\n\n");
 
- 
- 
-  int plansza[12][12];
-  int a,b;
- 
+    int plansza[12][12];
+    int a, b;
 
+    // initialize board with zeros
+    for(a = 0; a < 12; a++)
+    {
+        for(b = 0; b < 12; b++)
+        {
+            plansza[a][b] = 0;
+        }
+    }
 
- 
- 
-  for(a=0;a<12;a++)
-  {
-                   for(b=0;b<12;b++)
-                   {
-                                    plansza[a][b]=0;
-                                   /* printf("%i ",plansza[a][b]);   */
-                    }
-                  // printf("\n");   
-                
-   }
-  printf("ustalenie wartosci pol na planszy\n");
-  printf("0- puste/zakryte pole\n");
-  
- printf("9 mina");
-  printf("1-8 ilosc min wokol pola");
-  
- 
+    printf("Board legend:\n");
+    printf("X - hidden cell\n");
+    printf("0 - empty cell\n");
+    printf("9 - mine\n");
+    printf("1-8 - number of mines around the cell\n\n");
 
+    int e, f, g;
 
- 
-  int e,f,g;
-  
-  
-  //miny
+    // mines
     srand(time(0));
-  for(g=0;g<10;g++)
-  {
-  
-   e= rand() % 10 + 1;
-   f=rand() % 10 + 1;
-   plansza[e][f]=9; 
-   
-  if(plansza[e+1][f]!=9)
-      plansza[e+1][f]++;
-      
-      if(plansza[e-1][f-1]!=9)
-      plansza[e-1][f-1]++;
-      
-      if(plansza[e+1][f+1]!=9)
-       plansza[e+1][f+1]++;
-       
-       if(plansza[e][f-1]!=9)
-         plansza[e][f-1]++;
-         
-         if(plansza[e][f+1]!=9)
-          plansza[e][f+1]++;
-          
-          if(plansza[e+1][f-1]!=9)
-          plansza[e+1][f-1]++;
-          
-          if(plansza[e-1][f+1]!=9)
-           plansza[e-1][f+1]++;
-           
-           if(plansza[e-1][f]!=9)
-            plansza[e-1][f]++;
-   }
- /*  for(a=0;a<12;a++)
-  {
-                   for(b=0;b<12;b++)
-                   {
-                                    
-                                    printf("%i ",plansza[a][b]);   
-                    }
-                   printf("\n");   
-                
-   }*/
-   
-   printf("\n");
- 
+   for(g = 0; g < 10; g++)
+{
+    e = rand() % 10 + 1;
+    f = rand() % 10 + 1;
 
-    
-int j,k;
-int planszadwa[10][10];
-   for(j=0;j<10;j++)
-  {
-                   for(k=0;k<10;k++)
-                   {
-                                    
-                                   planszadwa[j][k]=0;
-                                  
-                                    printf("%i ",planszadwa[j][k]);
-                                    
-                    }
-                    printf("\n");
-                
-   }
-   int l;
-   for(l=0;l<90;l++)
-   {
-   
-  printf("podaj wspolrzedne pola\n");
-  
-   int h,i;
-    scanf("%i",&h);
-    scanf("%i",&i);
-   
-   for(j=0;j<10;j++)
-  {
-                   for(k=0;k<10;k++)
-                   {
-                                    
-                                
-                                   planszadwa[h][i]=plansza[h+1][i+1];
-                                    printf("%i ",planszadwa[j][k]);
-                                    
-                    }
-                    printf("\n");
-                
-   }
-   if(planszadwa[h][i]==9)
-   {printf("mina przegrales");
-   l=101;}
-   if(l==90)
-printf("wygrales");
-   
+    // prevent duplicate mine placement
+    if(plansza[e][f] == 9)
+    {
+        g--;        // repeat this iteration
+        continue;
+    }
+
+    plansza[e][f] = 9;
+
+    if(plansza[e+1][f] != 9)
+        plansza[e+1][f]++;
+
+    if(plansza[e-1][f-1] != 9)
+        plansza[e-1][f-1]++;
+
+    if(plansza[e+1][f+1] != 9)
+        plansza[e+1][f+1]++;
+
+    if(plansza[e][f-1] != 9)
+        plansza[e][f-1]++;
+
+    if(plansza[e][f+1] != 9)
+        plansza[e][f+1]++;
+
+    if(plansza[e+1][f-1] != 9)
+        plansza[e+1][f-1]++;
+
+    if(plansza[e-1][f+1] != 9)
+        plansza[e-1][f+1]++;
+
+    if(plansza[e-1][f] != 9)
+        plansza[e-1][f]++;
 }
 
- 
-  system("PAUSE");   
-  return 0;
+    int j, k;
+    int planszadwa[10][10];
+
+    // initialize visible board as hidden (-1)
+    for(j = 0; j < 10; j++)
+    {
+        for(k = 0; k < 10; k++)
+        {
+            planszadwa[j][k] = -1;
+            printf("X ");
+        }
+        printf("\n");
+    }
+
+    int l;
+
+    for(l = 0; l < 90; l++)
+    {
+        printf("\nEnter coordinates (row and column 0-9):\n");
+
+        int h, i;
+        scanf("%i", &h);
+        scanf("%i", &i);
+
+        // reveal chosen cell
+        planszadwa[h][i] = plansza[h+1][i+1];
+
+        // print board
+        for(j = 0; j < 10; j++)
+        {
+            for(k = 0; k < 10; k++)
+            {
+                if(planszadwa[j][k] == -1)
+                    printf("X ");
+                else
+                    printf("%i ", planszadwa[j][k]);
+            }
+            printf("\n");
+        }
+
+        if(planszadwa[h][i] == 9)
+        {
+            printf("\nGame over! You hit a mine.\n");
+            l = 101;
+        }
+
+        if(l == 90)
+        {
+            printf("\nCongratulations! You won.\n");
+        }
+    }
+
+    system("PAUSE");
+    return 0;
 }
